3e03d22d498dc6f63f64e1f16d1d1f070caaf9bb
Thiago Paiva <thiagohdaqw@gmail.com>
2022-07-19 23:19:41 -0300

    Adicionando efeito drop

--- main.pl
+++ main.pl
@@ -3,7 +3,7 @@ drawChar(Char, Count) :- write(Char), X is Count - 1, drawChar(Char, X).
 
 
 drawLine(_, X, _, Width) :- X == Width, !.
-drawLine(Char, X, Y, Width) :- grid(X,Y,CElem), write(CElem), plus(X,1,XX), drawLine(Char, XX, Y, Width).
+drawLine(Char, X, Y, Width) :- grid(X,Y,CElem), write(CElem), plus(X,1,XX), drawLine(Char, XX, Y, Width), !.
 drawLine(Char, X, Y, Width) :- write(Char), plus(X,1,XX), drawLine(Char, XX, Y, Width).
 
 
@@ -11,18 +11,34 @@ draw(_, Height, Y) :- Y == Height, !.
 draw(Width, Height, Y) :- drawLine('_',0,Y,Width), nl, plus(Y,1,YY), draw(Width, Height, YY).
 
 
+drawGrid :- draw(20,10,0).
+
+clear :- drawChar('\n',80).
+
+
 player(0,Char) :- Char = 'X'.
 player(1,Char)  :- Char = 'O'.
 
-main(Turn) :-   write('Informe o X: '), read(X),
-                write('Informe o Y: '), read(Y),
+
+dropColumn(Row,[]) :- Row is 9.
+dropColumn(Row,Column) :- last(Column,C), Row is C-1.
+
+drop(Column,Row) :- findall(Y,grid(Column,Y,_), C), dropColumn(Row,C).
+
+
+main(Turn) :-   write('Player['),write(Turn),write('] - Informe a coluna: '),
+                read(Column),
+                drop(Column,Row),
                 player(Turn, Char),
-                assert(grid(X,Y,Char)),
-                draw(20,10,0),
+                assert(grid(Column,Row,Char)),
+                clear,
+                drawGrid,
                 T is (Turn+1) mod 2,
                 main(T).
 
 
-:-  drawChar('\n',80),
+:-  dynamic(grid/3),
+    clear,
+    drawGrid,
     main(0),
     halt.
