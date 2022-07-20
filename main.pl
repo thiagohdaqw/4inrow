74ce5db2fa6880f4602d7bd05c35a60aa3530e25
Thiago Paiva <thiagohdaqw@gmail.com>
2022-07-19 22:25:13 -0300

    Adicionando turnos

--- main.pl
+++ main.pl
@@ -3,7 +3,7 @@ drawChar(Char, Count) :- write(Char), X is Count - 1, drawChar(Char, X).
 
 
 drawLine(_, X, _, Width) :- X == Width, !.
-drawLine(Char, X, Y, Width)  :- grid(X, Y,CElem), write(CElem), plus(X,1,XX), drawLine(Char, XX, Y, Width).
+drawLine(Char, X, Y, Width) :- grid(X,Y,CElem), write(CElem), plus(X,1,XX), drawLine(Char, XX, Y, Width).
 drawLine(Char, X, Y, Width) :- write(Char), plus(X,1,XX), drawLine(Char, XX, Y, Width).
 
 
@@ -11,13 +11,18 @@ draw(_, Height, Y) :- Y == Height, !.
 draw(Width, Height, Y) :- drawLine('_',0,Y,Width), nl, plus(Y,1,YY), draw(Width, Height, YY).
 
 
-main :- write('Informe o X: '), read(X),
-        write('Informe o Y: '), read(Y),
-        assert(grid(X,Y,'X')),
-        draw(20, 10, 0),
-        main.
+player(0,Char) :- Char = 'X'.
+player(1,Char)  :- Char = 'O'.
+
+main(Turn) :-   write('Informe o X: '), read(X),
+                write('Informe o Y: '), read(Y),
+                player(Turn, Char),
+                assert(grid(X,Y,Char)),
+                draw(20,10,0),
+                T is (Turn+1) mod 2,
+                main(T).
 
 
 :-  drawChar('\n',80),
-    main,
+    main(0),
     halt.
