57477f4fc35918a406187464fe5483e41a02cc65
Thiago Paiva <thiagohdaqw@gmail.com>
2022-07-19 23:27:40 -0300

    Adicionando footer numerico

--- main.pl
+++ main.pl
@@ -2,16 +2,19 @@ drawChar(_, Count) :- Count =< 0, !.
 drawChar(Char, Count) :- write(Char), X is Count - 1, drawChar(Char, X).
 
 
-drawLine(_, X, _, Width) :- X == Width, !.
-drawLine(Char, X, Y, Width) :- grid(X,Y,CElem), write(CElem), plus(X,1,XX), drawLine(Char, XX, Y, Width), !.
-drawLine(Char, X, Y, Width) :- write(Char), plus(X,1,XX), drawLine(Char, XX, Y, Width).
+drawLine(_, X, _, Width) :- X == Width, !, nl.
+drawLine(Char, X, Y, Width) :- grid(X,Y,CElem), write(CElem), write(' '), plus(X,1,XX), drawLine(Char, XX, Y, Width), !.
+drawLine(Char, X, Y, Width) :- write(Char), write(' '), plus(X,1,XX), drawLine(Char, XX, Y, Width).
+
+drawFooter(X,Width) :- X > Width, !, nl.
+drawFooter(X,Width) :- write(X), write(' '), plus(X,1,XX), drawFooter(XX, Width).
 
 
-draw(_, Height, Y) :- Y == Height, !.
-draw(Width, Height, Y) :- drawLine('_',0,Y,Width), nl, plus(Y,1,YY), draw(Width, Height, YY).
+draw(Width, Height, Y) :- Y == Height, !, drawFooter(1,Width), nl.
+draw(Width, Height, Y) :- drawLine('_',0,Y,Width), plus(Y,1,YY), draw(Width, Height, YY).
 
 
-drawGrid :- draw(20,10,0).
+drawGrid :- draw(10,10,0).
 
 clear :- drawChar('\n',80).
 
@@ -27,7 +30,8 @@ drop(Column,Row) :- findall(Y,grid(Column,Y,_), C), dropColumn(Row,C).
 
 
 main(Turn) :-   write('Player['),write(Turn),write('] - Informe a coluna: '),
-                read(Column),
+                read(X),
+                Column is X-1,
                 drop(Column,Row),
                 player(Turn, Char),
                 assert(grid(Column,Row,Char)),
