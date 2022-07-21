5a284b0f64866d6fb355f08a61e26071571ed49f
Thiago Paiva <thiagohdaqw@gmail.com>
2022-07-20 22:15:52 -0300

    Iniciando gui

--- main.pl
+++ main.pl
@@ -1,20 +1,25 @@
+:- use_module(library(pce)).
+
 drawChar(_, Count) :- Count =< 0, !.
 drawChar(Char, Count) :- write(Char), X is Count - 1, drawChar(Char, X).
 
 
-drawLine(_, X, _, Width) :- X == Width, !, nl.
-drawLine(Char, X, Y, Width) :- grid(X,Y,CElem), write(CElem), write(' '), plus(X,1,XX), drawLine(Char, XX, Y, Width), !.
-drawLine(Char, X, Y, Width) :- write(Char), write(' '), plus(X,1,XX), drawLine(Char, XX, Y, Width).
+drawLine(_, X, _, Width, _) :- X == Width, !, nl.
+drawLine(Char, X, Y, Width, P) :-
+    XXX is X*50, YYY is Y*50,
+    send(P,display,new(CI,circle(50)),point(XXX,YYY)),
+    plus(X,1,XX),
+    drawLine(Char, XX, Y, Width, P).
 
 drawFooter(X,Width) :- X > Width, !, nl.
 drawFooter(X,Width) :- write(X), write(' '), plus(X,1,XX), drawFooter(XX, Width).
 
 
-draw(Width, Height, Y) :- Y == Height, !, drawFooter(1,Width), nl.
-draw(Width, Height, Y) :- drawLine('_',0,Y,Width), plus(Y,1,YY), draw(Width, Height, YY).
+draw(Width, Height, Y, P) :- Y == Height, !.
+draw(Width, Height, Y, P) :- drawLine('_',0,Y,Width,P), plus(Y,1,YY), draw(Width, Height, YY,P).
 
 
-drawGrid :- draw(10,10,0).
+drawGrid(P) :- draw(10,10,0,P).
 
 clear :- drawChar('\n',80).
 
@@ -29,20 +34,27 @@ dropColumn(Row,Column) :- last(Column,C), Row is C-1.
 drop(Column,Row) :- findall(Y,grid(Column,Y,_), C), dropColumn(Row,C).
 
 
-main(Turn) :-   write('Player['),write(Turn),write('] - Informe a coluna: '),
+main(Turn,P) :- write('Player['),write(Turn),write('] - Informe a coluna: '),
                 read(X),
                 Column is X-1,
                 drop(Column,Row),
                 player(Turn, Char),
                 assert(grid(Column,Row,Char)),
                 clear,
-                drawGrid,
+                drawGrid(P),
                 T is (Turn+1) mod 2,
-                main(T).
+                main(T,P).
 
+initGui(P):-
+    new(P,picture),
+    new(D, window('4 IN A ROW')),
+    send(D, size, size(500, 500)),
+    send(D, display, P, point(0, 0)),
+    send(P,width(500)),
+    send(P,height(500)),
+    drawGrid(P),
+    send(D, open).
 
 :-  dynamic(grid/3),
-    clear,
-    drawGrid,
-    main(0),
-    halt.
+    initGui(P),
+    main(0,P).
